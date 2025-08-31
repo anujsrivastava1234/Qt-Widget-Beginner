ffac8d7c03ab19715b4f808dbd3f8056b3fae70c
anuj <anujsrivastava1596@gmail.com>
2025-08-31 10:31:51 +0530

    Update main.cpp

--- Section-3/3-2SmartPointers/main.cpp
+++ Section-3/3-2SmartPointers/main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <memory.h>
 #include "house.h"
 
 void do_stuff()
@@ -10,9 +11,22 @@ void do_stuff()
     */
 
     //Heap
+    /*
     House *p_h1 = new House(nullptr, "Second");
     p_h1->print_info();
     delete p_h1;
+    */
+
+    //! Smart Pointers
+    // Unique Pointers
+    std::unique_ptr<House> up_prt1(new House(nullptr, "Third"));
+    up_prt1->print_info();
+
+    std::unique_ptr<House> up_ptr2 = up_prt1;
+
+    //Shared Pointer
+    std::shared_ptr<House> shr_ptr1(new House(nullptr, "Fourth"));
+    shr_ptr1->print_info();
 
 }
 
